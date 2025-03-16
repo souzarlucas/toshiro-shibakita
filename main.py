9a3bc2d6a4f4a4e690a2ee865cfa655640bc97ad
Lucas Souza <lucas.rsza@proton.me>
2025-03-15 23:03:29 -0400

    Commit inicial

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     pygame.init()  # Inicializa todos os módulos do pygame
     game = Game()
     game.run()
-    pygame.quit()  # Encerra todos os módulos do pygame
+    pygame.quit()  # Encerra todos os módulos do git branch
